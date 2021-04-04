5fe2faab92503f27598c5db8d177e023f4a2f42c
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-04-04 19:14:45 +0100

    counter display built tdd

--- features/counter/components/counterDisplay/index.test.js
+++ features/counter/components/counterDisplay/index.test.js
@@ -2,11 +2,23 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import CounterDisplay from './index'
 
-describe('CounterDisplay', () => {
-    it('should display title passed by props', () => {
-        const testTitle = 'test title'
-        render(<CounterDisplay title={testTitle} />)
+const TEST_TITLE = 'test title'
+const TEST_COUNT = 58
 
-        expect(screen.getByText(testTitle)).toBeInTheDocument()
+describe('CounterDisplay', () => {
+    const props = {
+        title: TEST_TITLE,
+        count: TEST_COUNT
+    }
+
+    it('should display title passed by props', () => {
+        render(<CounterDisplay title={TEST_TITLE} {...props} />)
+
+        expect(screen.getByText(TEST_TITLE)).toBeInTheDocument()
+    })
+    it('should display count passed by props', () => {
+        render(<CounterDisplay count={TEST_COUNT} {...props} />)
+
+        expect(screen.getByText(TEST_COUNT)).toBeInTheDocument()
     })
 })
--- features/counter/components/counterDisplay/index.tsx
+++ features/counter/components/counterDisplay/index.tsx
@@ -3,12 +3,14 @@ import { Typography } from '@material-ui/core'
 
 interface Props {
     title: string
+    count: number
 }
 
-export const CounterDisplay: React.FC<Props> = ({title}) => {
+export const CounterDisplay: React.FC<Props> = ({title, count}) => {
     return (
         <div>
             <Typography>{title}</Typography>
+            <Typography>{count}</Typography>
         </div>
     )
 }
