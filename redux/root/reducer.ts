d4a47a30514e0e4b90e8f9ceb26398c8c96c0674
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-04-04 15:51:51 +0100

    actions and reducer build out for root

---
+++ redux/root/reducer.ts
@@ -0,0 +1,22 @@
+import { combineReducers, Reducer } from 'redux'
+import { HYDRATE } from 'next-redux-wrapper'
+import counterState from '../../features/counter/redux/reducer'
+import * as actions from './actions'
+
+const appReducer: Reducer = combineReducers({
+    counterState
+})
+
+function rootReducer(state = {}, action: Action): Reducer {
+    switch (action.type) {
+        case HYDRATE:
+            const nextState = {
+                ...state,
+                ...action.payload
+            }
+            case actions.CLEAR:
+                return appReducer({}, action)
+            default:
+                return (appReducer(state, action))
+    }
+}
