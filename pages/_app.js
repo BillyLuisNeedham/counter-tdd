ec62fa60ca9977a0b44c89464497d547521bde35
BillyLuisNeedham <billyluisneedham@gmail.com>
2021-04-04 16:31:52 +0100

    imported useStore to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import "../styles/globals.css"
 import { Provider } from "react-redux"
+import { useStore } from '../redux/store'
 
 function MyApp({ Component, pageProps }) {
   //extract redux state overrides from page props
   const { counterState } = pageProps
 
   //initialize redux store with overrides
-  const store = userStore({ counterState })
+  const store = useStore({ counterState })
 
   return (
     <Provider store={store}>
